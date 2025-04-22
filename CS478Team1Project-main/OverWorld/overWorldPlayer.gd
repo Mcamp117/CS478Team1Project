2eee9253be751fe53ec3f2b17d2919c6ab8a597c
k-9810 <92935972+k-9810@users.noreply.github.com>
2025-04-21 20:21:34 -0500

    fixed overworld sprite not turning left/right, and fixed enemy hitbox

--- CS478Team1Project-main/OverWorld/overWorldPlayer.gd
+++ CS478Team1Project-main/OverWorld/overWorldPlayer.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 @export var speed: float = 10
-
+@onready var characterSprite = $AnimatedSprite2D
 func _physics_process(_delta):
 	$Label.text=str(Player.Health)
 	var dirInput = Vector2(
@@ -9,4 +9,8 @@ func _physics_process(_delta):
 	)
 	print(dirInput)
 	velocity = dirInput * speed
+	if velocity.x<0:
+		characterSprite.flip_h = true
+	else:
+		characterSprite.flip_h = false
 	move_and_slide()
